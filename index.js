f37f24ca26fc1c2a21b2df7b40eb10468548bc1d
FELIX-MAIN <117909509+FELIX-MAIN@users.noreply.github.com>
2022-11-24 22:13:44 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
 // Code your solution here
+// Code your solution here
+function findMatching(){
+    const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+const getdriver = drivers.find(driver => `driver.name === 'Bobby', 'Sammy'`);
+
+console.log(getdriver);
+}
+findMatching('Bobby', 'Bobby');
+findMatching('Sammy');
+
+//fuzzyMatch
+function fuzzyMatch(){
+    const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'bobby']
+    drivers.push(drivers);
+
+    console.log('drivers');
+}
+fuzzyMatch('drivers');
+
+//matchname
+function matchName(){
+    let drivers = [
+        {
+          name: 'Bobby',
+          hometown: 'Pittsburgh' },
+        {
+          name: 'Sammy',
+          hometown: 'New York' } ,
+        {
+          name: 'Sally',
+          hometown: 'Cleveland' },
+        {
+          name: 'Annette',
+          hometown: 'Los Angeles' },
+        {
+          name: 'Bobby',
+          hometown: 'Tampa Bay' }
+      ];
+drivers = drivers.filter(driver => driver.hometown == 'pittsburgh');
+console.log(drivers);
+    }
+matchName();
